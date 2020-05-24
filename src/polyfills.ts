5e3ea303b7a3525db2f8508c745c8817cb0b735f
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2020-05-24 19:39:53 +0200

    Remove support for IE11

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,10 +1,2 @@
-import 'babel-polyfill'
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
-
-import 'proxy-polyfill'
-
-import { WebpMachine } from 'webp-hero'
-
-const webpMachine = new WebpMachine()
-webpMachine.polyfillDocument()
