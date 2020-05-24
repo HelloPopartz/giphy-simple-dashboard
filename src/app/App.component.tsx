b4f48280621803464bb474bfcc30ce7f14f2d7b2
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2020-05-24 10:53:21 +0200

    Added documentation of libraries

--- src/app/App.component.tsx
+++ src/app/App.component.tsx
@@ -6,7 +6,8 @@ import { AppLibraryProvider } from './AppLibraryProvider.component'
 import { AppRoutes } from './AppRoutes.component'
 
 export function App() {
-  // Initialize once
+  // Initialize redux store -> https://es.redux.js.org/
+  // Instead of declaring as a singleton, declaring the redux store real time usually prevents some circular dependencies in Jest
   const store = useMemo(configureStore, [])
   return (
     <StoreProvider store={store}>
--- src/app/AppLibraryProvider.component.tsx
+++ src/app/AppLibraryProvider.component.tsx
@@ -7,6 +7,8 @@ import { historyManager } from 'services/history'
 export type AppLibraryProps = WithChildren
 
 // Necessary providers and libraries for App
+// @material-ui/styles for the styling solution -> https://material-ui.com/es/styles/basics/
+// React-router for routing -> https://reacttraining.com/react-router/web/guides/quick-start
 export function AppLibraryProvider({ children }: AppLibraryProps) {
   const theme = useMemo(createTheme, [])
   return (
