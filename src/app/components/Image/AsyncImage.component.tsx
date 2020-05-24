71cd2efc335e9c9cd94951fe00bbae2eeb11f642
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2020-05-24 10:15:01 +0200

    Add AsyncImage, without animations

---
+++ src/app/components/Image/AsyncImage.component.tsx
@@ -0,0 +1,42 @@
+import { makeStyles } from '@material-ui/styles'
+import React, { Suspense } from 'react'
+import { useImage } from 'react-image'
+import { AppTheme } from 'services/theme'
+
+import clsx from 'clsx'
+
+import { Spinner } from '../Loading'
+
+export type AsyncImageProps = React.ImgHTMLAttributes<HTMLImageElement> & {
+  spinnerColor?: string
+}
+
+function AsyncImageInternal({ src, srcSet, alt = '', ...otherProps }: AsyncImageProps) {
+  const { src: processedSrc } = useImage({
+    srcList: src || srcSet || '',
+  })
+  return <img src={processedSrc} alt={alt} {...otherProps} />
+}
+
+export function AsyncImage({ className, spinnerColor, ...otherProps }: AsyncImageProps) {
+  const classes = useStyles()
+  const spinnerComponent = (
+    <div className={clsx(className, classes.spinnerContainer)}>
+      <Spinner size={30} singleColor={spinnerColor} />
+    </div>
+  )
+  return (
+    <Suspense fallback={spinnerComponent}>
+      <AsyncImageInternal className={className} {...otherProps} />
+    </Suspense>
+  )
+}
+
+const useStyles = makeStyles(({ spacing }: AppTheme) => ({
+  spinnerContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: spacing(3),
+  },
+}))
