763b95113e25bcedfd1f7d6fd8a0b2bbd24048b9
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2020-05-24 19:02:09 +0200

    Change order of elements in images

--- src/app/entities/giphy/giphy.helpers.ts
+++ src/app/entities/giphy/giphy.helpers.ts
@@ -1,11 +1,11 @@
 import { GiphyData, WithDimensions } from './giphy.types'
 
 export function getGiphyLivePreviewUrl({ images }: GiphyData): string[] {
-  return [images.preview_webp.url, images.preview.url, images['480w_still'].url]
+  return [images.preview.url, images.preview_webp.url, images['480w_still'].url]
 }
 
 export function getGiphyImageUrl({ images }: GiphyData): string[] {
-  return [images.original.url, images.original_mp4.url, images.original_still.url]
+  return [images.original_mp4.url, images.original.url, images.original_still.url]
 }
 
 export function getGiphyProportions({ images }: GiphyData): WithDimensions {
