6361e04a80354ae363b2d33003dfb3338104a761
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2020-05-24 19:25:44 +0200

    Test of polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,11 +1,10 @@
-import 'proxy-polyfill'
-
+import 'babel-polyfill'
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 
-import 'core-js/features/array/find'
-import 'core-js/features/object/assign'
-import 'core-js/features/promise'
-import 'core-js/features/string/ends-with'
-import 'core-js/features/symbol/for'
-import 'core-js/features/weak-set'
+import 'proxy-polyfill'
+
+import { WebpMachine } from 'webp-hero'
+
+const webpMachine = new WebpMachine()
+webpMachine.polyfillDocument()
