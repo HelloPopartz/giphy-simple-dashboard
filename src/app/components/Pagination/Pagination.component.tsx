5199f22d23f87f553e01ba835cc34e339fef9fa1
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2020-05-24 10:34:57 +0200

    Add pagination component

---
+++ src/app/components/Pagination/Pagination.component.tsx
@@ -0,0 +1,70 @@
+import clsx from 'clsx'
+import React from 'react'
+import ReactPaginate, { ReactPaginateProps } from 'react-paginate'
+
+import ChevronLeftIcon from 'mdi-react/ChevronLeftIcon'
+import ChevronRightIcon from 'mdi-react/ChevronRightIcon'
+import { makeStyles, AppTheme } from 'services/theme'
+import { ExtendableStyles } from 'utils/react'
+
+import { IconButton } from '../Buttons'
+
+export type PaginationProps = ExtendableStyles &
+  Pick<ReactPaginateProps, 'pageCount' | 'pageRangeDisplayed' | 'marginPagesDisplayed' | 'forcePage' | 'onPageChange'>
+
+export function Pagination({ className, ...otherProps }: PaginationProps) {
+  const classes = useStyles()
+  const previousLabel = (
+    <IconButton>
+      <ChevronLeftIcon />
+    </IconButton>
+  )
+  const nextLabel = (
+    <IconButton>
+      <ChevronRightIcon />
+    </IconButton>
+  )
+  return (
+    <ReactPaginate
+      containerClassName={clsx(className, classes.paginationContainer)}
+      previousLabel={previousLabel}
+      nextLabel={nextLabel}
+      pageLinkClassName={classes.pageClassName}
+      disabledClassName={classes.disabledClassName}
+      breakClassName={classes.pageClassName}
+      activeClassName={classes.activePage}
+      disableInitialCallback
+      {...otherProps}
+    />
+  )
+}
+
+const useStyles = makeStyles(({ spacing, typography }: AppTheme) => ({
+  paginationContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: 0,
+    listStyle: 'none',
+  },
+  pageClassName: {
+    listStyle: 'none',
+    cursor: 'pointer',
+    fontFamily: typography.family,
+    fontSize: typography.sizes.M,
+    textAlign: 'center',
+    padding: spacing(),
+    marginLeft: spacing(),
+    marginRight: spacing(),
+    ['&:hover']: {
+      fontWeight: typography.weights.bold,
+    },
+  },
+  activePage: {
+    fontWeight: typography.weights.bold,
+  },
+  disabledClassName: {
+    opacity: 0.3,
+    pointerEvents: 'none',
+    cursor: 'not-allowed',
+  },
+}))
