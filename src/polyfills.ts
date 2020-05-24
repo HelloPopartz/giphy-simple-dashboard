5ad2af2f6c147f098e18cddd7faff950874ab500
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2020-05-24 18:52:02 +0200

    Add IE polyfills

---
+++ src/polyfills.ts
@@ -0,0 +1,11 @@
+import 'proxy-polyfill'
+
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
+import 'core-js/features/array/find'
+import 'core-js/features/object/assign'
+import 'core-js/features/promise'
+import 'core-js/features/string/ends-with'
+import 'core-js/features/symbol/for'
+import 'core-js/features/weak-set'
